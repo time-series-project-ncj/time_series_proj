5c6bfa7ba0bf12e4a4ccc0e86828c12b91058ad3
Nicole Garza <Nicole.Garza410@gmail.com>
2019-04-24 19:08:28 -0500

    preds for two weeks

--- acquire.py
+++ acquire.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import csv
 
-def merge_dataframes():
+def merge_activities():
     with open('2018-04-26_through_2018-05-26.csv') as fd:
         reader=csv.reader(fd)
         interestingrows=[row for idx, row in enumerate(reader) if idx in range(35,67)]
@@ -40,3 +40,52 @@ def merge_dataframes():
     df_final = df_final.rename(columns={0: "date", 1: 'calories_burned', 2: 'steps', 3: 'distance', 4: ' floors', 5: 'minutes_sedentary', 6: 'minutes_lightly_active',7: 'minutes_fairly_active', 8: 'minutes_very_active',9: 'activity_calories'})
     df_final.drop([0], inplace=True)
     return df_final
+
+# def merge_foods():
+#     with open('2018-04-26_through_2018-05-26.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(1,33)]
+#     df = pd.DataFrame(interestingrows)
+#     with open('2018-05-27_through_2018-06-26.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df1 = pd.DataFrame(interestingrows)
+#     with open('2018-06-27_through_2018-07-27.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df2 = pd.DataFrame(interestingrows)
+#     with open('2018-07-28_through_2018-08-26.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df3 = pd.DataFrame(interestingrows)
+#     with open('2018-08-27_through_2018-09-26.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df4 = pd.DataFrame(interestingrows)
+#     with open('2018-09-27_through_2018-10-27.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df5 = pd.DataFrame(interestingrows)
+#     with open('2018-10-28_through-2018-11-27.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df6 = pd.DataFrame(interestingrows)
+#     with open('2018-11-28_through_2018-12-28.csv') as fd:
+#     reader=csv.reader(fd)
+#     interestingrows=[row for idx, row in enumerate(reader) if idx in range(2,33)]
+#     df = pd.DataFrame(interestingrows)
+#     df7 = pd.DataFrame(interestingrows)
+#     frames = [df, df1, df2, df3, df4, df5, df6, df7]
+#     df_final = pd.concat(frames)
+#     df_final = df_final.rename(columns={0: "date", 1: 'calories_in'})
+#     df_final.drop([0], inplace=True)
+#     return df_final
+
+# def merge_foodlog():
+    
--- prepare.py
+++ prepare.py
@@ -22,6 +22,17 @@ def add_minutes_active_column(df):
     df['minutes_activity'] = df.minutes_lightly_active + df.minutes_fairly_active + df.minutes_very_active
     return df
 
-
+def merge_yhats():
+    forecast_minactive.rename(index=str, columns={'yhat':'minutes_active_pred', 'ds':'date'}, inplace=True)
+    forecast_minsedentary.rename(index=str, columns={'yhat':'minutes_sedentary_pred','ds':'date1'}, inplace=True)
+    forecast_steps.rename(index=str, columns={'yhat':'steps_pred','ds':'date2'}, inplace=True)
+    forecast_calories_burned.rename(index=str, columns={'yhat':'calories_burned_pred','ds':'date3'}, inplace=True)
+    frames = [forecast_minactive, forecast_minsedentary, forecast_steps, forecast_calories_burned]
+    df_preds = pd.concat(frames, axis=1)
+    df_preds = df_preds.drop(['date1','date1','date2','date3'], axis=1)
+    df_preds.set_index('date', inplace=True)
+    df_preds = df_preds.tail(14)
+    df_preds.to_csv('fitbit_preds.csv')
+    return df_preds
 
 
